19c86b5631efd6283cac17278cc0170acef0e196
darkwizz <psyhoge@gmail.com>
2022-10-11 06:56:54 +0200

    add manually created natural language prompts for further training

--- utils/data_preprocess.py
+++ utils/data_preprocess.py
@@ -4,7 +4,29 @@ import spacy
 _pl_nlp = spacy.load('pl_core_news_lg')
 
 QUESTION_TYPES = ['Gap filling', 'Yes/no', 'Multiple choice', 'Living named entity',
+
 'Named entity', 'Numeric', 'Proper noun', 'Rest']
+QUESTION_PROMPTS = {
+    QUESTION_TYPES[0]: ['Proszę uzupełnić lukę: |&|'],
+    QUESTION_TYPES[1]: ['Proszę podać odpowiedź "tak" czy "nie": |&|'],
+    QUESTION_TYPES[2]: ['Proszę wybrać poprawną opcję z kilku podanych: |&|', 'Zastanawiam się nad tym, którą odpowiedź z podanych jest poprawną. Proszę pomóc z pytaniem: |&|'],
+    QUESTION_TYPES[3]: ['Dane pytanie jest o żywym podmiocie. Proszę powiedzieć, |&|'],
+    QUESTION_TYPES[4]: ['To pytanie jest po prostu o podmiocie nazwanym. Proszę powiedzieć, |&|'],
+    QUESTION_TYPES[5]: ['Prosze podać liczbę: |&|', 'W tym pytaniu odpowiedź zawiera liczbę. Proszę powiedzieć, |&|'],
+    QUESTION_TYPES[6]: ['W odpowiedzi na dane pytanie jest nazwa własna. Proszę powiedzieć: |&|'],
+    QUESTION_TYPES[7]: ['Proszę odpowiedzieć na pytanie niżej: |&|', 'Zawsze zastanawiałem się: |&|', 'Jaka jest odpowiedź na następujące pytanie: |&|', 'Jest pytanie: |&|']
+}
+
+ANSWER_PROMPTS = [
+    'Odpowiedź na dane pytanie to |&|',
+    'Poprawna odpowiedź: |&|',
+    'Proszę podać odpowiedź: |&|'
+]
+
+QUESTION_PREFIX = '<QUESTION>'
+QUESTION_SUFFIX = '</QUESTION>'
+ANSWER_PREFIX = '<ANSWER>'
+ANSWER_SUFFIX = '</ANSWER>'
 
 
 def split_data_by_filters(data, **filters):
