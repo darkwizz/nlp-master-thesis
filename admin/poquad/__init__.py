b28ded0812cb92315f8378709cf675ef6f43075b
darkwizz <psyhoge@gmail.com>
2022-11-10 01:51:06 +0100

    move general admin commands implementation into admin/__init__.py, add flags for question preprocessing (no implementation)

--- admin/poquad/__init__.py
+++ admin/poquad/__init__.py
@@ -10,6 +10,10 @@ def prepare_arg_parser(subparsers: _SubParsersAction):
     me_group: _MutuallyExclusiveGroup = parser.add_mutually_exclusive_group(required=True)
     me_group.add_argument('-t', '--target_path', help='destination path of the processed dataset. Saved in the PolEval format')
     me_group.add_argument('-c', '--count_token', action='store_true', help='count tokens in the dataset. Ignores any other preprocessing flag')
+    process_me_group: _MutuallyExclusiveGroup = parser.add_mutually_exclusive_group()
+    process_me_group.add_argument('-p', '--prompts', action='store_true', help='append prompts to all questions and answers from the same pool')
+    process_me_group.add_argument('-g', '--gprompts', action='store_true', help='split questions on groups and each group has its own pool of prompts')
+    process_me_group.add_argument('-a', '--artificial', action='store_true', help='enclose questions and answers by an artificial suffix and a prefix')
     parser.add_argument('-i', '--impossible', action='store_true', help='include tokens with is_impossible in "true"')
     parser.add_argument('-e', '--engine', dest='tokenizer', default='spacy', help='used with -c flag. Tokenizer which is used to split dataset texts on tokens. Default is spaCy (case insensitive), but also can be passed a path to a Transformers tokenizer')
     return parser
