3c0cdc3c4faa43054e2b7ce53050e17a13188332
darkwizz <psyhoge@gmail.com>
2022-11-12 22:31:04 +0100

    remove all extra preprocessing from tokenizers

--- papugapt2/utils.py
+++ papugapt2/utils.py
@@ -9,10 +9,10 @@ EOS_TOKEN = '</s>'
 def get_gpt2_tokenizer_function(tokenizer, max_question_len=45, max_answer_len=10):
     def tokenize(examples):
         if 'answer' in examples:
-            prompts = [f'pytanie: {q}\nOdpowiedź: {a}{EOS_TOKEN}' for q, a in zip(examples['question'], examples['answer'])]
+            prompts = [f'{q}\n{a}{EOS_TOKEN}' for q, a in zip(examples['question'], examples['answer'])]
             max_len = max_question_len + max_answer_len
         else:
-            prompts = [f'pytanie: {q}\nOdpowiedź: ' for q in examples['question']]
+            prompts = [f'{q}\n' for q in examples['question']]
             max_len = max_question_len
         results = tokenizer(prompts, padding='max_length', max_length=max_len)
         return results
