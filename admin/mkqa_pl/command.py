402b3b560fff292672f79410f20589f20d70ff0a
darkwizz <psyhoge@gmail.com>
2022-11-19 15:50:50 +0100

    implement a separate function for reading MKQA .tsv files

--- admin/mkqa_pl/command.py
+++ admin/mkqa_pl/command.py
@@ -4,6 +4,29 @@ from admin import ANSWER_FEATURE, QUESTION_FEATURE
 from utils.data_preprocess import get_artificially_augmented_dataset, get_prompt_augmented_dataset
 
 
+def load_mkqa_subset(base_dir):
+    from datasets import Dataset
+
+    in_path = os.path.join(base_dir, 'in.tsv')
+    expected_path = os.path.join(base_dir, 'expected.tsv')
+
+    result = []
+    in_file = open(in_path)
+    expected_file = open(expected_path)
+    for in_line, expected_line in zip(in_file, expected_file):
+        formatted_in = in_line.strip()
+        formatted_expected = expected_line.strip()
+        if all((formatted_in, formatted_expected)):
+            question, answer_type = formatted_in.split('\t')
+
+            answer_parts = formatted_expected.split('\t')
+            answer = answer_parts[0]
+            alternatives = answer_parts[1:] if len(answer_parts) > 1 else []
+
+            result.append({'question': question, 'answer': answer, 'alternatives': alternatives, 'answer_type': answer_type})
+    return Dataset.from_list(result)
+
+
 def print_number_of_tokens(data, tokenizer):
     if not tokenizer:
         print('A tokenizer must be specified')
@@ -18,7 +41,8 @@ def print_number_of_tokens(data, tokenizer):
 def main(args):
     from utils import compose_subsets_paths
     from utils.mkqa_pl import download_pl_subset, save_dataset
-    from utils.workflow import load_datasets, save_data
+    from utils.workflow import save_data
+    from datasets import DatasetDict
 
     print('Inside mkqa_pl subset main')
     seed = args.seed
@@ -28,7 +52,11 @@ def main(args):
     else:
         data_base_dir = args.directory
         subset_dirs = compose_subsets_paths(data_base_dir)
-        data = load_datasets(seed=seed, **subset_dirs)
+        subsets_dict = {}
+        for subset, path in subset_dirs.items():
+            subset_data = load_mkqa_subset(path)
+            subsets_dict[subset] = subset_data
+        data = DatasetDict(subsets_dict)
     
     if args.count_token:
         print_number_of_tokens(data, args.tokenizer)
