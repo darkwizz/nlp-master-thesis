9dd5e36bf8f8f66dd6f20820107555416976f8bd
darkwizz <psyhoge@gmail.com>
2022-11-27 02:34:40 +0100

    use early stopping for 4-, 5-, 6- poquad sbatch scripts

--- sbatch-scripts/plt5-poquad/train/6-all-prompting.sh
+++ sbatch-scripts/plt5-poquad/train/6-all-prompting.sh
@@ -6,7 +6,7 @@
 #SBATCH -n 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --mem 32GB
-#SBATCH -t 24:00:00
+#SBATCH -t 00:35:00
 
 module load python/3.9.6-gcccore-11.2.0
 module load cuda/11.6.0
@@ -15,4 +15,4 @@ source $SCRATCH/venv/bin/activate
 cd $SCRATCH/t5-gpt2-scripts
 export TRANSFORMERS_CACHE="$SCRATCH/transformers-cache"
 
-python main.py -n plt5 -r baseline -b ./data-iterations/6-all-prompting -t azwierzc/plt5-large-poquad -m azwierzc/plt5-large-poquad --results-dir ./6-plt5-poquad-results --save-pretrained -q "103" -a "153" --test-max-length "23" --test-batch-size "256" -M ./plt5-poquad-checkpoints/baseline/6-trained-model -o ./training-log/6-plt-poquad
+python main.py -n plt5 -r early_stopping -b ./data-iterations/6-all-prompting -t azwierzc/plt5-large-poquad -m azwierzc/plt5-large-poquad --results-dir ./6-plt5-poquad-results --save-pretrained -q "103" -a "153" --test-max-length "23" --test-batch-size "256" -M ./plt5-poquad-checkpoints/early_stopping/6-trained-model -o ./training-log/6-plt-poquad --patience 2
