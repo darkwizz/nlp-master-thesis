af87fcce9b28a2917cfe75948928bace3fa14614
darkwizz <psyhoge@gmail.com>
2022-11-27 02:00:08 +0100

    update sbatch 1-, 2- and 3- scripts to use early_stopping

--- sbatch-scripts/plt5-poquad/train/1-train-base-mkqa.sh
+++ sbatch-scripts/plt5-poquad/train/1-train-base-mkqa.sh
@@ -6,7 +6,7 @@
 #SBATCH -n 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --mem 32GB
-#SBATCH -t 04:00:00
+#SBATCH -t 00:20:00
 
 module load python/3.9.6-gcccore-11.2.0
 module load cuda/11.6.0
@@ -15,4 +15,4 @@ source $SCRATCH/venv/bin/activate
 cd $SCRATCH/t5-gpt2-scripts
 export TRANSFORMERS_CACHE="$SCRATCH/transformers-cache"
 
-python main.py -n plt5 -r baseline -b ./data-iterations/1-base-mkqa -t azwierzc/plt5-large-poquad -m azwierzc/plt5-large-poquad --results-dir ./1-plt5-poquad-results --save-pretrained -q "45" -a "30" --test-max-length "16" --test-batch-size "256" -M ./plt5-poquad/baseline/trained-model
+python main.py -n plt5 -r early_stopping -b ./data-iterations/1-base-mkqa -t azwierzc/plt5-large-poquad -m azwierzc/plt5-large-poquad --results-dir ./1-plt5-poquad-results --save-pretrained -q "45" -a "30" --test-max-length "16" --test-batch-size "256" -M ./plt5-poquad/early_stopping/1-trained-model --patience 2
